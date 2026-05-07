e274d78c65dc03798cae2afa1ce754f0ea640cab
Bowen Jiang <eternalaxholo28@gmail.com>
2026-05-07 09:08:10 -0400

    Setup JavaFX and Ikonli dependencies

--- env-manager/src/main/java/com/apcs/Main.java
+++ env-manager/src/main/java/com/apcs/Main.java
@@ -1,7 +1,31 @@
 package com.apcs;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.kordamp.ikonli.javafx.FontIcon;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        FontIcon icon = new FontIcon("mdi-home");
+        icon.setIconSize(48);
+
+        Label label = new Label("Hello world!");
+        
+        VBox root = new VBox(10, icon, label);
+        root.setStyle("-fx-alignment: center; -fx-padding: 20;");
+        
+        Scene scene = new Scene(root, 300, 200);
+        primaryStage.setTitle("JavaFX Test");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
 }
