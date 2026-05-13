e5ff715ae20920ac508c8c50665d915deca22aae
MonsterV82152 <eddyliang193@gmail.com>
2026-05-13 12:23:19 -0400

    feat(icon): added icon

--- env-manager/src/main/java/com/apcs/Main.java
+++ env-manager/src/main/java/com/apcs/Main.java
@@ -1,7 +1,20 @@
 package com.apcs;
 
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            SystemTray tray = SystemTray.getSystemTray();
+            Image icon = Toolkit.getDefaultToolkit().getImage(
+                    Main.class.getResource("/com/apcs/icons/icons8-key-50.png") // your resource path
+            );
+            TrayIcon trayIcon = new TrayIcon(icon, "ENV Password Manager");
+            tray.add(trayIcon);
+            trayIcon.setImageAutoSize(true); // scales to fit the menu bar
+            System.out.println("System tray supported: " + SystemTray.isSupported());
+        } catch (AWTException e) {
+            System.err.println("System tray not supported: " + e.getMessage());
+        }
     }
 }
