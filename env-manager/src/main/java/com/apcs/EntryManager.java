264933e143f4b9e57d1fcc36d3ec4eb9e7590c56
Bowen Jiang <eternalaxholo28@gmail.com>
2026-05-13 12:40:11 -0400

    core: set up core modules, updated pom.xml for dependensies, and set up EnvEntry.java and EnvEntry.class

---
+++ env-manager/src/main/java/com/apcs/EntryManager.java
@@ -0,0 +1,18 @@
+package com.apcs;
+
+public class EntryManager {
+    // This class will manage the entries for the password manager
+    // It will handle adding, removing, and retrieving entries from the database
+    public void addEntry(String UUID) {
+        // Code to add an entry to the database
+    }
+
+    public void removeEntry(String UUID) {
+        // Code to remove an entry from the database
+    }
+
+    public String getEntry(String UUID) {
+        // Code to retrieve an entry from the database
+        return null; // Placeholder return value
+    }
+}
