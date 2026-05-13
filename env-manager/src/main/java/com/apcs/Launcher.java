f5c74cb7049ce8b3893591ca2524df118b12f5fc
MonsterV82152 <eddyliang193@gmail.com>
2026-05-13 12:26:21 -0400

    removed launcher

--- env-manager/src/main/java/com/apcs/Launcher.java
+++
@@ -1,8 +0,0 @@
-package com.apcs;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Main.main(args);
-        System.out.println("Hello world from Launcher!");
-    }
-}
